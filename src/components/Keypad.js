1cd66b97af9363446e3bf8a308fa64a29fc6c080
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-29 11:32:48 +0300

    adds event listeners

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange(event) {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input onChange={handleChange} type="password"></input>
+    </div>
+  );
 }
 
 export default Keypad;
